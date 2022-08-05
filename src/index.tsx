96b5886d1ba226af5aa2b8d9805c3b8ca1040795
Berzan <berzan@tutanota.com>
2022-08-05 13:15:28 +0300

    add: source

---
+++ src/index.tsx
@@ -0,0 +1,36 @@
+import React, { ComponentPropsWithRef, ElementType, forwardRef } from "react"
+
+// Props is the type for the component props
+
+type Props<C extends ElementType> =
+    JSX.IntrinsicAttributes &
+    JSX.LibraryManagedAttributes<C, { className?: string } & Omit<ComponentPropsWithRef<C>, 'className'>>
+
+
+
+// Refs is the type for ForwardedRef's
+// It returns both a RefObject & a RefCallback
+// So there is a typing error while assigning a RefObject
+// If you know how to solve it, please open a pull request
+
+type Refs<C extends ElementType> = ComponentPropsWithRef<C>['ref']
+
+
+
+// "wrapn()" function takes any HTML tag as an argument
+// It return another function which takes a TemplateStringsArray argument
+// TemplateStringsArray includes the TailwindCSS classes you passed
+//
+// For example;
+//
+//    const MyButton = wrapn('button')`
+//        bg-sky-400 text-white
+//   `
+//
+// It assigns (TemplateStringsArray + ' ' + className) to className prop of the component
+
+export function wrapn<C extends ElementType>(Tag: C) {
+    return (tw: TemplateStringsArray) => forwardRef<Refs<C>, Props<C>>(
+        (p, ref) => <Tag ref={ref} {...p} className={tw + ' ' + p.className} />
+    )
+}
