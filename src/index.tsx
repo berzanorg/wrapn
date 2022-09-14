ca8c192875ac3548a92c7a2745d762e0137da6c2
Berzan <berzan@tutanota.com>
2022-09-14 19:47:39 +0300

    fix: props type error

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,33 @@
-import React, { DetailedHTMLProps, HTMLAttributes, SVGProps, forwardRef } from "react"
+import React, { DetailedHTMLProps, HTMLAttributes, SVGProps, forwardRef, ElementType, ComponentPropsWithoutRef, ForwardRefExoticComponent, PropsWithoutRef } from "react"
 
 
 // It extends all the keys of `JSX.IntrinsicElements`
 type HTMLTag = keyof JSX.IntrinsicElements
 
+
 // Type of the props of given HTMLTag
-type WProps<C extends HTMLTag> = JSX.IntrinsicElements[C]
+type WProps<C extends ElementType> = 
+    JSX.IntrinsicAttributes & JSX.LibraryManagedAttributes<C, ComponentPropsWithoutRef<C>>
+
 
 // Type of the ref of given HTMLTag
-type WRef<C extends HTMLTag> =
-    WProps<C> extends DetailedHTMLProps<HTMLAttributes<infer E>, infer E> ? E :
-    WProps<C> extends SVGProps<infer S> ? S : never
+type WRef<C extends ElementType> =
+    C extends HTMLTag ? 
+        WProps<C> extends DetailedHTMLProps<HTMLAttributes<infer E>, infer E> ? E :
+        WProps<C> extends SVGProps<infer S> ? S : never
+    : never
+
+
+// Type of the `wrapn` function
+type WrapnFunction = <T extends ElementType>(Tag: T) => WrapnStyleFunction<T>
+
+
+// Type of the `wrapn` style function
+type WrapnStyleFunction<T extends ElementType> = (tw: TemplateStringsArray) => WrapnComponent<T>
+
+
+// Type of a `wrapn` component
+type WrapnComponent<T extends ElementType> = ForwardRefExoticComponent<PropsWithoutRef<WProps<T>> & React.RefAttributes<WRef<T>>>
 
 
 /**
@@ -35,10 +52,10 @@ type WRef<C extends HTMLTag> =
  * ```
  * Visit [the repo](https://github.com/wrapn/wrapn).
  */
-export const wrapn = <T extends HTMLTag>(Tag: T) => {
-    return (tw: TemplateStringsArray) => {
+export const wrapn: WrapnFunction = <T extends ElementType>(Tag: T): WrapnStyleFunction<T> => {
+    return (tw: TemplateStringsArray): WrapnComponent<T> => {
         return forwardRef<WRef<T>, WProps<T>>(
-            ({ className, ...props }, ref) => <Tag ref={ref} {...props} className={`${tw} ${className || ''}`}/>
+            (props, ref) => <Tag ref={ref} {...props} className={`${tw} ${props.className || ''}`}/>
         )
     }
 }
