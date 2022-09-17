cd0847d18a2d0af047a068c73dfc13a966a0e0b3
Berzan <berzan@tutanota.com>
2022-09-17 16:41:28 +0300

    improve: wrapn function

--- src/index.tsx
+++ src/index.tsx
@@ -1,60 +1,117 @@
-import React, { DetailedHTMLProps, HTMLAttributes, SVGProps, forwardRef, ElementType, ComponentPropsWithoutRef, ForwardRefExoticComponent, PropsWithoutRef } from "react"
-
-
-// It extends all the keys of `JSX.IntrinsicElements`
-type HTMLTag = keyof JSX.IntrinsicElements
-
-
-// Type of the props of given HTMLTag
-type WProps<C extends ElementType> = 
-    JSX.IntrinsicAttributes & JSX.LibraryManagedAttributes<C, ComponentPropsWithoutRef<C>>
-
-
-// Type of the ref of given HTMLTag
-type WRef<C extends ElementType> =
-    C extends HTMLTag ? 
-        WProps<C> extends DetailedHTMLProps<HTMLAttributes<infer E>, infer E> ? E :
-        WProps<C> extends SVGProps<infer S> ? S : never
-    : never
-
-
-// Type of the `wrapn` function
-type Wrapn = <T extends ElementType>(Tag: T) => WrapnFunction<T>
-
-
-// Type of the `wrapn` style function
-type WrapnFunction<T extends ElementType> = (tw: TemplateStringsArray) => WrapnComponent<T>
-
-
-// Type of a `wrapn` component
-type WrapnComponent<T extends ElementType> = ForwardRefExoticComponent<PropsWithoutRef<WProps<T>> & React.RefAttributes<WRef<T>>>
+import React, { ComponentPropsWithRef, DetailedHTMLProps, forwardRef, ForwardRefExoticComponent, HTMLAttributes, ReactHTML } from "react";
 
 
 /**
- * # wrapn
- * Wrap any HTML Element into a styled component
- * - The function takes an HTML tag
- * - Returns another function which takes Tailwind classes
- * - Returns a component which has Tailwind classes embedded
+ * ## ⛸️ Wrapn ([repo](https://github.com/wrapn/wrapn))
  * 
- * ## Usage
+ * `WrapnTag` represents all the HTML elements.
+ * 
+ * For example: `"a"`, `"button"`, `"div"`, `"input"`, etc...
+ */
+export type WrapnTag = keyof ReactHTML
+
+
+/**
+ * ## ⛸️ Wrapn ([repo](https://github.com/wrapn/wrapn))
+ * 
+ * `Wrapn<(WrapnTag)>` represents wrapped components.
+ * 
+ * For example: `Wrapn<"a">`, `Wrapn<"button">`, `Wrapn<"div">`, etc...
+ */
+export type Wrapn<T extends WrapnTag> = ForwardRefExoticComponent<React.PropsWithoutRef<React.ComponentPropsWithRef<T>> & React.RefAttributes<WrapnRef<T>>>
+
+
+/**
+ * ## ⛸️ Wrapn ([repo](https://github.com/wrapn/wrapn))
+ * 
+ * `WrapnProps<(WrapnTag)>` represents the `props` of HTML elements.
+ * 
+ * For example: `WrapnProps<"a">`, `WrapnProps<"button">`, etc...
+ */
+export type WrapnProps<T extends WrapnTag> = ComponentPropsWithRef<T>
+
+
+/**
+ * ## ⛸️ Wrapn ([repo](https://github.com/wrapn/wrapn))
+ * 
+ * `WrapnRef<(WrapnTag)>` represents the `refs` of HTML elements.
+ * 
+ * For example: `WrapnRef<"a">`, `WrapnRef<"button">`, `WrapnRef<"div">`, etc...
+ */
+export type WrapnRef<T extends WrapnTag> = JSX.IntrinsicElements[T] extends DetailedHTMLProps<HTMLAttributes<infer R>, infer R> ? R : never
+
+
+
+// It represents `wrapn` function itself
+type WrapnFunction = <T extends WrapnTag>(Tag: T | Wrapn<T>) => WrapnStyleFunction<T>
+
+// It represents the style function that takes a `TemplateStringsArray`
+type WrapnStyleFunction<T extends WrapnTag> = (tw: TemplateStringsArray) => Wrapn<T>
+
+
+/**
+ * ## ⛸️ Wrapn ([repo](https://github.com/wrapn/wrapn))
+ * Wrap any HTML Element into a styled component!
+ * 
+ * - ⏱️ Create the fastest components using Tailwind.
+ * - 🎾 Autocomplete provides a joyful experience.
+ * - 🌯 Only an HTML element, but a styled one.
+ * 
+ * 
+ * ### Wrap an HTML tag
  * ```jsx
- * // A new wrapn component
- * const Button = wrapn('button')`
+ * // <MyButton/>
+ * const MyButton = wrapn('button')`
  *     h-12 px-6
  *     text-white
- *     bg-red-600
- *     hover:bg-red-500
+ *     bg-blue-500
  * `
- * // It is ready to use
- * <Button>Click</Button>
  * ```
- * Visit [the repo](https://github.com/wrapn/wrapn).
+ * 
+ * 
+ * ### Wrap a component
+ * ```jsx
+ * // Create a base component
+ * const ButtonBase = wrapn('button')`
+ *     h-12 px-6
+ * `
+ * // <ButtonPrimary/>
+ * const ButtonPrimary = wrapn(ButtonBase)`
+ *     bg-blue-500
+ * `
+ * // <ButtonSecondary/>
+ * const ButtonSecondary = wrapn(ButtonBase)`
+ *     bg-gray-500
+ * `
+ * ```
+ * 
+ * 
+ * ### TailwindCSS Intellisense
+ * - Install Tailwind CSS IntelliSense Extension to VS Code.
+ * - Press Ctrl + Shift + P keys at the same time.
+ * - Type Open User Settings (JSON) in the search box.
+ * - Put the content below inside the settings object.
+ * 
+ * `------------------------------------------------`
+ * ```json
+ * "editor.quickSuggestions": {
+ *      "strings": true
+ * },
+ * "tailwindCSS.experimental.classRegex": [
+ *      "wrapn\\(.*?\\)`([^`]*)"
+ * ],
+ * ```
+ * `------------------------------------------------`
+ * 
+ * - Done! You can now use Tailwind CSS IntelliSense.
+ * 
+ * ### Made with ❤️ by Wrapn contributors...
  */
-export const wrapn: Wrapn = <T extends ElementType>(Tag: T): WrapnFunction<T> => {
-    return (tw: TemplateStringsArray): WrapnComponent<T> => {
-        return forwardRef<WRef<T>, WProps<T>>(
-            (props, ref) => <Tag ref={ref} {...props} className={`${tw} ${props.className || ''}`}/>
+
+export const wrapn: WrapnFunction = <T extends WrapnTag>(Tag: T | Wrapn<T>): WrapnStyleFunction<T> => {
+    return (tw: TemplateStringsArray): Wrapn<T> => {
+        return forwardRef<WrapnRef<T>, WrapnProps<T>>(
+            (props, ref) => <Tag {...{ref,  className: `${tw} ${props.className || ''}`, ...props} as any}/>
         )
     }
 }
